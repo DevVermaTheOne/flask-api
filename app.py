fc583ba96e6ce82aea65bd0a399a0472ffd089f9
dev <devvermatheone@gmail.com>
2024-09-21 19:21:25 +0530

    variable routing

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="templates")
 
 @app.route('/')
-
 def home():
-    return "Hello, World!"
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return "This is the about page"
+
+@app.route('/user/<user_name>')
+def show_user_profile(user_name):
+    return user_name
 
 app.run(port=5000)
