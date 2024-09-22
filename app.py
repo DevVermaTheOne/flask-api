51e330f0ee2489eb52c14225d38c518d84cf19a6
dev <devvermatheone@gmail.com>
2024-09-22 20:32:37 +0530

    restful initiated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from flask_restful import Resource, Api
 
 app = Flask(__name__,template_folder="templates")
 
@@ -15,4 +15,20 @@ def about():
 def show_user_profile(user_name):
     return user_name
 
-app.run(port=5000)
+#api
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'data':'Hello, World!'}
+
+class Helloname(Resource):
+    def get(self, name):
+        return {'data': 'Hello, {}'.format(name)}
+        
+api.add_resource(HelloWorld, '/helloworld') # get localhost:5000/helloworld --> Hello World
+api.add_resource(Helloname, '/helloworld/<string:name>') # get localhost:5000/helloworld/Person --> Hello Person
+
+
+if __name__=='__main__':
+    app.run(debug=True)
