13398b1b64ee43fafebecdbae6970fbdb249ebbd
dev <devvermatheone@gmail.com>
2024-09-25 19:57:54 +0530

    testing get functionality

--- app.py
+++ app.py
@@ -3,31 +3,25 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__,template_folder="templates")
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return "This is the about page"
-
-@app.route('/user/<user_name>')
-def show_user_profile(user_name):
-    return user_name
-
-#api
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'data':'Hello, World!'}
+todos = {
+    1: {"task":"Letter", "summary":"write a letter to your friend"},
+    2: {"task":"task 2", "summary":"summary of task 2"},
+    3: {"task":"task 3", "summary":"summary of task 3"},
+    4: {"task":"task 4", "summary":"summary of task 4"},
+}
 
-class Helloname(Resource):
-    def get(self, name):
-        return {'data': 'Hello, {}'.format(name)}
-        
-api.add_resource(HelloWorld, '/helloworld') # get localhost:5000/helloworld --> Hello World
-api.add_resource(Helloname, '/helloworld/<string:name>') # get localhost:5000/helloworld/Person --> Hello Person
+class Todo(Resource):
+    def get(self, todo_id):
+        return todos[todo_id]
+
+class Todos(Resource):
+    def get(self):
+        return todos
+    
+api.add_resource(Todo, "/todos/<int:todo_id>")
+api.add_resource(Todos, "/todos")
 
 
 if __name__=='__main__':
